a216519d9474fc1fe68ae36908d4fbaac9998166
Rotem Adar <154267575+RotAdar@users.noreply.github.com>
2023-12-29 23:21:36 +0200

    Update DamkaBoard.java

--- DamkaBoard.java
+++ DamkaBoard.java
@@ -3,6 +3,16 @@
  */
 public class DamkaBoard {
 	public static void main(String[] args) {
-		//// Put your code here
+	int x= Integer.parseInt(args[0]);
+	for (int i= 0; i < x; i= i + 2) { // A loop that prints 2 rows of x *
+	    System.out.println ();
+	    for (int t= 0; t < x; t++) { // A loop that prints a row of x *
+	        System.out.print ("* ");
+	    }
+	    System.out.println ();
+	    for (int p= 0; p < x; p++) { // A loop that prints an opposite row of x *
+	        System.out.print (" *");
+	    }
+	}
 	}
 }
