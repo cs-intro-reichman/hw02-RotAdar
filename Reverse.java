f43dd13bd50ab71dfd46ba8246d485325f683008
Rotem Adar <154267575+RotAdar@users.noreply.github.com>
2023-12-29 21:41:48 +0200

    Update Reverse.java

--- Reverse.java
+++ Reverse.java
@@ -4,6 +4,17 @@
  */
 public class Reverse {
 	public static void main (String[] args){
-		//// Put your code here
+	String a = args[0];
+	int length = a.length(); 
+	String bwd = "";
+	char middle = 0;
+	for (int i = 0; i < (length); i++)
+	{
+	if (i == (length / 2) && (length % 2) == 0) middle = a.charAt(i-1);
+	if (i == (length / 2) && (length % 2) == 1) middle = a.charAt(i);
+	bwd= a.charAt(i) + bwd;
+	}
+	System.out.println(bwd);
+	System.out.println(middle);
 	}
 }
