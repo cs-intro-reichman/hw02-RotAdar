c2a1bd6ee9deac108c2b0c9af2a3ada962b5cba0
Rotem Adar <154267575+RotAdar@users.noreply.github.com>
2024-01-02 09:58:29 +0200

    Update OneOfEachStats1.java

--- OneOfEachStats1.java
+++ OneOfEachStats1.java
@@ -49,6 +49,6 @@ public class OneOfEachStats1 {
 	System.out.println("Number of families with 2 children: " + two);
 	System.out.println("Number of families with 3 children: " + three);
 	System.out.println("Number of families with 4 children: " + four); 
-	System.out.println("The most common number of children is " + common);
+	System.out.println("The most common number of children is " + common + ".");
 	}
 }
