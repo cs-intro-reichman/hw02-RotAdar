9bda23e76d0e4ef564bbb86836c6ae681b2845a9
Rotem Adar <154267575+RotAdar@users.noreply.github.com>
2024-01-02 10:03:51 +0200

    Update OneOfEachStats1.java

--- OneOfEachStats1.java
+++ OneOfEachStats1.java
@@ -45,7 +45,7 @@ public class OneOfEachStats1 {
    if (four >= three && four >= two) {
    	common= 4;
    }
-	System.out.println("Average: " + avg + " children to get at least one of each gender");
+	System.out.println("Average: " + avg + " children to get at least one of each gender.");
 	System.out.println("Number of families with 2 children: " + two);
 	System.out.println("Number of families with 3 children: " + three);
 	System.out.println("Number of families with 4 children: " + four); 
