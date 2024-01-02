795b38ae0996a210a0f3a9fd2117dfd49eb3cda1
Rotem Adar <154267575+RotAdar@users.noreply.github.com>
2024-01-02 08:17:14 +0200

    Update OneOfEachStats1.java

--- OneOfEachStats1.java
+++ OneOfEachStats1.java
@@ -6,6 +6,49 @@
  */
 public class OneOfEachStats1 {
 	public static void main (String[] args) {
-		//// Put your code here
+		int t= Integer.parseInt(args[0]);
+		double avg= 0.0;
+		int two= 0;
+		int three= 0;
+		int four= 0;
+
+		for (int i= 0; i < t; i++) {
+	boolean gender= true; //OneOfEach program starts (without printing)
+	boolean secGender= true;
+	int amount= 1;
+	if (Math.random() > 0.5) { // A loop that get a randon gender 
+	    gender= false;
+	    secGender= false;
+        }
+	while (gender == secGender) {
+	   	amount++;
+	    if (Math.random() < 0.5) {
+	       secGender= true;
+	       }
+        else {
+	    	secGender= false;
+           }
+	    }
+	        if (amount == 2) two++;
+	        if (amount == 3) three++;
+	        if (amount >= 4) four++;
+	        avg= avg + ((double)(amount));
+	}
+	avg= (avg / ((double)(t)));
+	int common= 0;
+	if (two >= three && two >= four) {
+       common= 2; 
+       }
+    if (three >= four && three >= two) {
+       		common= 3;
+       }
+   if (four >= three && four >= two) {
+   	common= 4;
+   }
+	System.out.println("Average: " + avg + " children to get at least one of each gender");
+	System.out.println("Number of families with 2 children: " + two);
+	System.out.println("Number of families with 3 children: " + three);
+	System.out.println("Number of families with 4 children: " + four); 
+	System.out.println("The most common number of children is " + common);
 	}
 }
